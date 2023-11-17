98f0e5de6716bfc7de3199f9696dd283c154bc28
taloy2 <sinabiananrey@gmail.com>
2023-11-17 14:40:52 +0800

    lamp

---
+++ src/Lamp/Lamp.java
@@ -0,0 +1,27 @@
+package Lamp;
+
+public class Lamp {
+
+    boolean is_on;
+
+    void turnOn() {
+        System.out.println("light's on!");
+    }
+
+    void turnOff() {
+        System.out.println("light's off!");
+
+    }
+}
+class main{
+    
+public static void main (String [] args){
+    Lamp led = new Lamp();
+    Lamp Halogen = new Lamp();
+    led.turnOff();
+    Halogen.turnOn();
+    
+}
+
+
+}
---
+++ src/Lamp/Students.java
@@ -0,0 +1,19 @@
+package Lamp;
+
+public class Students {
+
+    String name;
+    int roll_num;
+
+    
+}
+class ans{
+    public static void main(String[] args) {
+    Students student = new Students();
+    student.name="Rey";
+    student.roll_num= 20212010;
+        System.out.println("Hi! my name is "+student.name + " and my id is #"+ student.roll_num);
+        
+    }
+
+}
