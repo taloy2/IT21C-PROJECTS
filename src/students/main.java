61d640f57fa533c46087ba7528238e62c31bb3fa
taloy2 <sinabiananrey@gmail.com>
2023-11-17 14:19:19 +0800

    kani

---
+++ src/students/main.java
@@ -0,0 +1,18 @@
+package students;
+
+public class main {
+    
+     public static void main(String [] args){
+    
+   students students = new students();
+    students.setName("Rey Sinabianan");
+     students.setSection("IT21C");
+     students.setMotto("balag bugo basta maro");
+    
+        System.out.println("Name: "+ students.name);
+        System.out.println("Section: "+ students.section);
+        System.out.println("Motto: "+ students.motto);
+    
+    }    
+    
+}
